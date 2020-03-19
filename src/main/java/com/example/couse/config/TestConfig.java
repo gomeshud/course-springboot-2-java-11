cc0dc6ce5a49524d6b45b922aa54545b02feb1e6
gomeshud <hgf1@ifal.edu.br>
2020-03-19 20:38:16 -0300

    tudo até 306.Category

--- src/main/java/com/example/couse/config/TestConfig.java
+++ src/main/java/com/example/couse/config/TestConfig.java
@@ -1,5 +1,6 @@
 package com.example.couse.config;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.example.couse.entities.Category;
+import com.example.couse.entities.Order;
 import com.example.couse.entities.User;
+import com.example.couse.entities.enums.OrderStatus;
+import com.example.couse.repositories.CategoryRepository;
+import com.example.couse.repositories.OrderRepository;
 import com.example.couse.repositories.UserRepository;
 
 @Configuration
@@ -16,15 +22,33 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null,"Eletronics");
+		Category cat2 = new Category(null,"Books");
+		Category cat3 = new Category(null,"Computers");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
+		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+
+		Order o1 = new Order(null, Instant.parse("2020-03-18T20:30:29Z"),OrderStatus.PAID,u1);
+		Order o2 = new Order(null, Instant.parse("2020-03-18T21:40:49Z"), OrderStatus.WAITING_PAYMANT ,u2);
+		Order o3 = new Order(null, Instant.parse("2020-03-18T22:25:29Z"), OrderStatus.WAITING_PAYMANT ,u1);
+		
 		
 		userRepository.saveAll(Arrays.asList(u1,u2));
+		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
 	}
 	
 	
-
 }
--- src/main/java/com/example/couse/entities/Category.java
+++ src/main/java/com/example/couse/entities/Category.java
@@ -1,48 +1,31 @@
 package com.example.couse.entities;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-
 @Entity
-@Table(name = "tb_user")
-public class User implements Serializable {	
+@Table(name= "tb_category")
+public class Category implements Serializable	 {
 	private static final long serialVersionUID = 1L;
-
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
-	private String email;
-	private String fone;
-	private String password;
 	
-	@JsonIgnore
-	@OneToMany(mappedBy = "client")
-	private List<Order> orders = new ArrayList<>();
-	
-	
-	public User() {
+	public Category() {
 		
 	}
 
-	public User(Long id, String name, String email, String fone, String password) {
+	public Category(Long id, String name) {
 		super();
 		this.id = id;
 		this.name = name;
-		this.email = email;
-		this.fone = fone;
-		this.password = password;
 	}
 
 	public Long getId() {
@@ -61,35 +44,6 @@ public class User implements Serializable {
 		this.name = name;
 	}
 
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getFone() {
-		return fone;
-	}
-
-	public void setFone(String fone) {
-		this.fone = fone;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
-	public List<Order> getOrders() {
-		return orders;
-	}
-	
-
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -106,7 +60,7 @@ public class User implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		User other = (User) obj;
+		Category other = (Category) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -114,8 +68,7 @@ public class User implements Serializable {
 			return false;
 		return true;
 	}
-
-
 	
-
+	
+	
 }
