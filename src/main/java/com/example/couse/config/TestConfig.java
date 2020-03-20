c64d70fdfc2abfb32839490463e221c1d89cfa85
gomeshud <hgf1@ifal.edu.br>
2020-03-19 23:51:25 -0300

    Payment, one-to-one association

--- src/main/java/com/example/couse/config/TestConfig.java
+++ src/main/java/com/example/couse/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.example.couse.entities.Category;
 import com.example.couse.entities.Order;
 import com.example.couse.entities.OrderItem;
+import com.example.couse.entities.Payment;
 import com.example.couse.entities.Product;
 import com.example.couse.entities.User;
 import com.example.couse.entities.enums.OrderStatus;
@@ -81,6 +82,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null,Instant.parse("2020-03-18T22:30:29Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 	}
 	
