830804b8dbe9de79b633b5a43339ae81bd625c5a
gomeshud <hgf1@ifal.edu.br>
2020-03-19 22:49:35 -0300

    OrderItem, many-to-many association

---
+++ src/main/java/com/example/couse/repositories/OrderItemRepository.java
@@ -0,0 +1,13 @@
+package com.example.couse.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.couse.entities.OrderItem;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
+
+	
+	
+
+}
